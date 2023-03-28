748cbeb5c21a8d5ebda4e899fd216b623dcf6d6c
hhs <user20@ip-172-31-22-99.ap-northeast-1.compute.internal>
2023-03-28 03:56:13 +0000

    update build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>hello 본인이름 </code> and save to reload.
+          Congratulations!!! Good Day^^
         </p>
         <a
           className="App-link"
